7b3fa213e5339dfcc179a9cabd2a3524c655bdd2
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-30 19:36:13 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "阿里云盘自动签到一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/aliyun-auto-signin-replit"
+nix-env -iA nixpkgs.wget
+wget -O main.zip https://github.com/ImYrS/aliyun-auto-signin/archive/refs/heads/main.zip
+nix-env -iA nixpkgs.unzip
+unzip main.zip
+mv aliyun-auto-signin-main/* .
+rm -rf aliyun-auto-signin-main/
+cp -r example.config.ini config.ini && cp -r app.py main.py
+pip install -r requirements.txt
+rm -rf example.config.ini
+rm -rf main.zip && rm -rf How-To-Use-Action.md
+rm -rf LICENSE
+echo "搭建完成！！！"
+echo "阿里云盘自动签到一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/aliyun-auto-signin-replit"
+echo "点击Run按钮，启动阿里云盘自动签到项目! 使用愉快!!!"
+rm -rf README.md
